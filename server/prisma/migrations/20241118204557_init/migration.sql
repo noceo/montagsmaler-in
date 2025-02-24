d6ed2574c703aa2d0d9f03899dc161bfab58f6e9
noceo <dev@paulscha.de>
2025-02-24 13:34:27 -0600

    removes existing db and migrations

--- server/prisma/migrations/20241118204557_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Session" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Session_name_key" ON "Session"("name");
